cf717f3a9993356edc25aaf153c8ad31619cb83f
Chen <ts-beck.chen@rakuten.com>
2020-02-10 18:22:35 +0900

    wip CardFooter

---
+++ src/components/Card/components/CardFooter/CardFooter.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export type CardFooterProps = {
+  imageUrl: string,
+  userName: string,
+  numberOfParticipants: number,
+  submissionDate: string
+};
+
+const CardFooter = ({imageUrl, userName, numberOfParticipants, submissionDate}) => {
+  const monthsAgo = 0;
+  return (<div>
+    
+  </div>);
+};
+
+export default CardFooter;
