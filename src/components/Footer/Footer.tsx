b3e93b6cbb26d2fa535f4c9a666d502817c54d4d
Chen <ts-beck.chen@rakuten.com>
2020-02-17 17:00:17 +0900

    wrap up footer implementation

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Footer.module.scss';
+import { stripLeadingSlash } from 'history/PathUtils';
 
 export type FooterItem = {
   name: string,
@@ -23,31 +24,35 @@ const Footer = ({contacts, infos, languages, termsLink, privacyLink} : FooterPro
 
   return (<footer className={styles.footer}>
     <div className={styles.mainFooterArea}>
-      <div>
-        <h6>Contact</h6>
-        <ul>
-          {contactItems}
-        </ul>
-      </div>
-      <div>
-        <h6>Infos</h6>
-        <ul>
-          {infoItems}
-        </ul>
-      </div>
-      <div>
-        <h6>Language</h6>
-        <ul>
-          {langaugeItems}
-        </ul>
+      <div className={styles.grid}>
+        <div className={styles.column}>
+          <h6>Contact</h6>
+          <ul>
+            {contactItems}
+          </ul>
+        </div>
+        <div className={styles.column}>
+          <h6>Infos</h6>
+          <ul>
+            {infoItems}
+          </ul>
+        </div>
+        <div className={styles.column}>
+          <h6>Language</h6>
+          <ul>
+            {langaugeItems}
+          </ul>
+        </div>
       </div>
     </div>
     <div className={styles.termsAndPrivacyArea}>
-      <div className='trademark'>© 2020 Pitchcard</div>
-      <div className={styles.termsAndPrivacyContainer}>
-        <a href={termsLink}>Terms</a>
-        <span></span>
-        <a href={privacyLink}>Privacy</a>
+      <div className={styles.row}> 
+        <div className={styles.trademark}>© 2020 Pitchcard</div>
+        <div className={styles.termsAndPrivacyText}>
+          <a href={termsLink}>Terms</a>
+          <span>·</span>
+          <a href={privacyLink}>Privacy</a>
+        </div>
       </div>
     </div>
   </footer>);
